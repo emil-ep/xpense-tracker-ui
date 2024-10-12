02f6b516455359f257685472325800eb406d0918
Emil Pradeep <emilpradeep@gmail.com>
2024-10-12 15:30:06 +0530

    integrated upload api

---
+++ src/api/fileApi.ts
@@ -0,0 +1,19 @@
+
+export const uploadStatement = async (selectedFile : File) : Promise<any> => {
+
+    const formData = new FormData();
+    formData.append('file', selectedFile);
+
+    const response  = await fetch('http://localhost:8080/v1/file/upload/statement', {
+        method: 'POST',
+        body: formData,
+        headers: {
+            'Authorization': `Bearer ${localStorage.getItem('authToken')}`,
+        }
+    });
+    if(!response.ok){
+        throw new Error('Failed to fetch expenses')
+    }
+    const responseData: any = await response.json();
+    return responseData;
+}
--- src/pages/expense/AddExpense.tsx
+++ src/pages/expense/AddExpense.tsx
@@ -1,12 +1,32 @@
-import React from "react"
+import React, { ChangeEvent, useState } from "react"
 import './AddExpense.css'
 import Stack from "../../components/Stack"
+import { uploadStatement } from "../../api/fileApi";
 
 export default function AddExpense(){
+
+    const [selectedFile, setSelectedFile] = useState<File | null>(null);
+
+    const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+        if (event.target.files && event.target.files.length > 0) {
+            setSelectedFile(event.target.files[0]); 
+        }
+    }
+
+    const handleUpload = () => {
+        if (!selectedFile) {
+            alert("Please select a file first!");
+            return;
+        }
+        uploadStatement(selectedFile);
+    }
+
+
     return (
         <Stack className="homeContainer" direction="column">
             <Stack className="uploadContainer" direction="row" justify="center">
-                <button className="uploadBtn">Upload your statement</button>
+                <input className="uploadBtn" type="file" onChange={handleFileChange} />
+                <button className="uploadBtn" onClick={handleUpload}>Upload your statement</button>
             </Stack>
             
         </Stack>
