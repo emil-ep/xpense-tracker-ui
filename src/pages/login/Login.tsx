e613da6226c194e121a46e68f1fad3883c601afc
Emil Pradeep <emilpradeep@gmail.com>
2024-12-28 12:19:10 +0530

    updated login page

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,21 +1,19 @@
 import React, { useState } from "react";
-import Stack from "../../components/Stack";
-import './login.css'
+import { Box, Typography, TextField, Button, Stack } from "@mui/material";
 import { signInV2 } from "../../api/authApi";
-import { toast } from 'react-toastify';
-import { useNavigate } from 'react-router-dom';
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 import { apiCaller } from "../../api/apicaller";
 
 export default function Login() {
-
-  const [username, setUsername] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
   const navigate = useNavigate();
 
-  async function tryLogin(){
-    try{
-      const response: any = await apiCaller(signInV2({username, password}));
-      localStorage.setItem('authToken', response.data.token);
+  async function tryLogin() {
+    try {
+      const response: any = await apiCaller(signInV2({ username, password }));
+      localStorage.setItem("authToken", response.data.token);
       toast("Login Success", {
         position: "top-right",
         autoClose: 5000,
@@ -26,8 +24,8 @@ export default function Login() {
         progress: undefined,
         theme: "dark",
       });
-      navigate('/home');
-    }catch(error){
+      navigate("/home");
+    } catch (error) {
       toast("Login Failed", {
         position: "top-right",
         autoClose: 5000,
@@ -42,39 +40,70 @@ export default function Login() {
   }
 
   return (
-    <Stack className="container" direction="row">
-      <Stack className="imageSection" direction="row" justify='center'>
-        <img src="/images/login_wallpaper.jpg" className="img"  alt="login_wallpaper"/>
-      </Stack>
-      <Stack className="formSection" direction="column" justify="center" align="center">
-        <Stack direction="column" align="flex-start" justify="center">
-          <label className="inputLabel" htmlFor="email">Email</label>
-          <input 
-            className="inputField"
-            id="email" 
-            type="email" 
-            value={username} 
-            onChange={(e) => {
-              setUsername(e.target.value)
-            }} 
-            placeholder="Enter your email" 
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        height: "100vh",
+        backgroundColor: "#f5f5f5",
+      }}
+    >
+      {/* Left Section with Image */}
+      <Box
+        sx={{
+          flex: 1.5,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          background: "url('/images/art-expense.png') no-repeat center center",
+          backgroundSize: "cover",
+        }}
+      ></Box>
+
+      {/* Right Section with Form */}
+      <Box
+        sx={{
+          flex: 1,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          padding: 4,
+          backgroundColor: "#fff",
+        }}
+      >
+        <Typography variant="h4" gutterBottom>
+          Login
+        </Typography>
+        <Stack
+          spacing={2}
+          sx={{ width: "100%", maxWidth: "400px" }}
+        >
+          <TextField
+            label="Email"
+            variant="outlined"
+            fullWidth
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
           />
-          <label className="inputLabel" htmlFor="password">Password</label>
-          <input 
-            className="inputField"
-            id="password" 
-            type="password" 
-            value={password} 
-            onChange={(e) => {
-              setPassword(e.target.value)
-            }} 
-            placeholder="Enter your password" 
+          <TextField
+            label="Password"
+            variant="outlined"
+            type="password"
+            fullWidth
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
-          <button className="submitButton" type="submit" onClick={tryLogin}>
+          <Button
+            variant="contained"
+            color="primary"
+            fullWidth
+            onClick={tryLogin}
+          >
             Submit
-          </button>
+          </Button>
         </Stack>
-      </Stack>
-    </Stack>
+      </Box>
+    </Box>
   );
 }
