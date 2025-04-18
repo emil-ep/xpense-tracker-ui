0051919b466c37f2f235cc992367b1c32e5d1c62
Emil <emilpradeep@gmail.com>
2025-04-18 18:30:00 +0530

    removed custom stack implementation

---
+++ src/pages/customDashboard/CustomDashboard.tsx
@@ -0,0 +1,10 @@
+
+
+export default function CustomDashboard() {
+    return (
+        <div>
+        <h1>Custom Dashboard</h1>
+        {/* Add your custom dashboard components here */}
+        </div>
+    );
+}
